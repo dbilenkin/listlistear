1ff2040b161d97e3d1f8e4941660ee0d3a00c089
dbilenkin <dbilenkin@gmail.com>
2018-03-13 10:08:48 -0400

    yep

--- src/App.js
+++ src/App.js
@@ -1,43 +1,89 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Game from "./Game"
+import Game from "./Game";
 
 function createNewGame() {
   return Math.floor(Math.random() * 100);
 }
 
-const ButtonToNavigate = ({ title, id, history }) => (
-  <button
-    type="button"
-    onClick={() => history.push('/game/' + id)}
-  >
+const HostButton = ({ title, id, history }) => (
+  <button type="button" onClick={() => history.push("/game/" + id)}>
     {title}
   </button>
 );
 
-const SomeComponent = () => (
-  <Route path="/" render={(props) => <ButtonToNavigate {...props} title="New Game" id={createNewGame()}/>} />
-)
-
-const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
-      <SomeComponent />
-      <hr />
-      <Route exact path="/" component={Home} />
-      <Route path="/game/:id" component={Game} />
-      <Route path="/topics" component={Topics} />
-    </div>
-  </Router>
+const JoinButton = (props) => (
+  <button
+    type="button"
+    onClick={() => props.history.push("/game/" + props.id)}
+  >
+    Join Game
+  </button>
 );
+
+const HostGame = () => (
+  <Route
+    path="/"
+    render={props => (
+      <HostButton {...props} title="Host Game" id={createNewGame()} />
+    )}
+  />
+);
+
+const JoinGame = (parentProps) => (
+  <div>
+    <label>
+      Game Id:
+      <input
+        type="text"
+        value={parentProps.id}
+        onChange={parentProps.onChange}
+      />
+    </label>
+    <Route
+      path="/"
+      render={props => <JoinButton {...props} id={parentProps.id}/>}
+    />
+  </div>
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameId: ""
+    };
+
+    this.handlePickGame = this.handlePickGame.bind(this);
+  }
+
+  handlePickGame(event) {
+    this.setState({ gameId: event.target.value });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+          <HostGame />
+          <JoinGame id={this.state.gameId} onChange={this.handlePickGame}/>
+          <hr />
+          <Route exact path="/" component={Home} />
+          <Route path="/game/:id" component={Game} />
+          <Route path="/topics" component={Topics} />
+        </div>
+      </Router>
+    );
+  }
+}
 
 const Home = () => (
   <div>
